f40f5f2f831cb2f30722567f5a24af94dcc0ec0a
Jorge <jorgitoovazquez@hotmail.com>
2018-10-16 20:51:40 +0200

    Clase gestion hibernate Changed

--- src/gestionHibernate.java
+++ src/gestionHibernate.java
@@ -1,42 +1,78 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.security.auth.login.Configuration;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.mysql.cj.xdevapi.SessionFactory;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+import java.util.Iterator;
+import java.util.List;
 
 public class gestionHibernate implements InterfazBBDD {
+	
+	Session session;
+	int ID;
+	String nombre;
+    String descripcion;
+    String caracteristica1;
+    String caracteristica2;
+    Marca marca;
 
 	@Override
 	public void Leer() throws SQLException, FileNotFoundException {
-		// TODO Auto-generated method stub
+    	
+		 Session session;
+
+	        System.out.print("Inicio de consulta simple empleados");
+	        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+	        Session s =sf.openSession();
+	        Query q = s.createQuery("Select e from Employee e");
+	        List results = q.list();
+	        Iterator empleadosIterator = results.iterator();
+
+	        while (empleadosIterator.hasNext()){
+
+	            Employee empleado = (Employee)empleadosIterator.next();
+	            System.out.print("name "+ empleado.getEmpName() + "\n" );
+
+	        }
 		
 	}
 
 	@Override
 	public void Escribir() throws SQLException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void TXTABBDD() throws FileNotFoundException, SQLException {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void BBDDTXT() throws SQLException, IOException {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void Actualizar() throws SQLException {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void Borrar() throws SQLException {
-		// TODO Auto-generated method stub
 		
 	}
 
