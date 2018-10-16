d1f151f264c941e4cbfa80c5a3fb45ac4a4c94ee
Jorge <jorgitoovazquez@hotmail.com>
2018-10-16 11:59:27 +0200

    Clase Hibernate creada

---
+++ src/gestionHibernate.java
@@ -0,0 +1,43 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class gestionHibernate implements InterfazBBDD {
+
+	@Override
+	public void Leer() throws SQLException, FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Escribir() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TXTABBDD() throws FileNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void BBDDTXT() throws SQLException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Actualizar() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Borrar() throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
