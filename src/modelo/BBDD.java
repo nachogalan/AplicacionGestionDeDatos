90498375809bf25482b558b512be332fdf60f293
Elias Periañez <33256248+eliasperianez@users.noreply.github.com>
2018-12-02 13:11:13 +0100

    BBDD Casi terminado

--- src/modelo/BBDD.java
+++ src/modelo/BBDD.java
@@ -14,8 +14,7 @@ ________________________________________________________________________________
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Properties;
@@ -28,74 +27,105 @@ public class BBDD implements Data {
 	private Conexion conexion;
 	private String tableCoche = "";
 	private String tableMarca = "";
-	Marca marca = new Marca(0, "", "");
-	Coche coche = new Coche(0, "", "", "", "", marca);
-	HashMap<String, Marca> marcaMap = new HashMap<>();
-	HashMap<String, Coche> cocheMap = new HashMap<>();
+
+	private final String[] columnasCoches = { "ID", "Nombre", "Descripcion", "Caracteristica1", "Caracteristica2",
+			"ID_Fabricante" };
+	private final String[] columnasMarcas = { "ID_Fabricante", "Nombre", "Sede" };
 
 	public BBDD() {
 
-			Properties p = new Properties();
-			try {
-				p.load(new FileInputStream("bbdd.ini"));
-				login = p.getProperty("Usuario");
-				pwd = p.getProperty("Contrasena");
-				tableCoche = p.getProperty("tablaCoche");
-				tableMarca = p.getProperty("tablaMarca");
-				bd = p.getProperty("db");
-				conexion = new Conexion(bd, login, pwd, true);
-				System.out.println(" - Conexión con MySQL establecida -");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		Properties p = new Properties();
+		try {
+			p.load(new FileInputStream("bbdd.ini"));
+			login = p.getProperty("Usuario");
+			pwd = p.getProperty("Contrasena");
+			tableCoche = p.getProperty("tablaCoche");
+			tableMarca = p.getProperty("tablaMarca");
+			bd = p.getProperty("db");
+			conexion = new Conexion(bd, login, pwd, true);
+			System.out.println(" - Conexión con MySQL establecida -");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	private String[] getArFromCar(Coche ch) {
+		return new String[] { Integer.toString(ch.getId()), ch.getNombre(), ch.getDescripcion(),
+				ch.getCaracteristica1(), ch.getCaracteristica2(), Integer.toString(ch.getMarca().getId_fabricante()) };
+	}
+
+	private String[] getArFromMarca(Marca mc) {
+		return new String[] {Integer.toString(mc.getId_fabricante()), mc.getNombre(), mc.getSede()};
 	}
 
 	@Override
 	public void updateCoche(Coche ch) {
-		//conexion.editarDatos(tableCoche, columnas, valores, condicion)
+		conexion.editarDatos(this.tableCoche, this.columnasCoches, this.getArFromCar(ch), "ID =" + ch.getId());
 	}
 
 	@Override
 	public void uploadCoche(Coche ch) {
-		// TODO Apéndice de método generado automáticamente
+		conexion.insertarDatos(this.tableCoche, this.columnasCoches, this.getArFromCar(ch));
 
 	}
 
 	@Override
 	public void deleteCoche(Coche ch) {
-		// TODO Apéndice de método generado automáticamente
-
+		conexion.borrarFila(this.tableCoche, "ID = " + ch.getId());
 	}
 
 	@Override
 	public HashMap<Integer, Coche> getCoche() {
-		// TODO Apéndice de método generado automáticamente
-		return null;
+		HashMap<Integer, Coche> result = new HashMap<Integer, Coche>();
+		ResultSet rs = conexion.Consulta("SELECT * FROM " + this.tableCoche);
+		try {
+			int i = 0;
+			while (rs.next()) {
+				ResultSet tmpRset = conexion.Consulta("SELECT * FROM "+this.tableCoche+" WHERE ID = "+rs.getInt(5));
+				tmpRset.next();
+				Marca mc = new Marca(tmpRset.getInt(0), tmpRset.getString(1), tmpRset.getString(2));
+				result.put(i, new Coche(rs.getInt(0),rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4), mc));
+				i++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public void updateMarca(Marca mc) {
-		// TODO Apéndice de método generado automáticamente
-
+		conexion.editarDatos(this.tableMarca, this.columnasMarcas, this.getArFromMarca(mc), "ID =" + mc.getId_fabricante());
 	}
 
 	@Override
 	public void uploadMarca(Marca mc) {
-		// TODO Apéndice de método generado automáticamente
+		conexion.insertarDatos(this.tableMarca, this.columnasMarcas, this.getArFromMarca(mc));
 
 	}
 
 	@Override
 	public void deleteMarca(Marca mc) {
-		// TODO Apéndice de método generado automáticamente
+		conexion.borrarFila(this.tableMarca, "ID = " + mc.getId_fabricante());
 
 	}
 
 	@Override
 	public HashMap<Integer, Marca> getMarca() {
-		// TODO Apéndice de método generado automáticamente
-		return null;
+		HashMap<Integer, Marca> result = new HashMap<Integer, Marca>();
+		System.out.println("SELECT * FROM " + this.tableCoche);
+		ResultSet rs = conexion.Consulta("SELECT * FROM " + this.tableCoche);
+		try {
+			int i = 0;
+			while (rs.next()) {
+				result.put(i, new Marca(rs.getInt(0), rs.getString(1), rs.getString(2)));
+				i++;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
