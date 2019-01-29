7d6aba3e2eaa862e0402dc930302458fe3a833c5
Ignacio Galan <fcta.1516.galani@gmail.com>
2019-01-29 13:01:12 +0100

    Leer Coches realizado

--- src/modelo/Mongo.java
+++ src/modelo/Mongo.java
@@ -13,6 +13,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import modelo.Marca;
 
 public class Mongo implements Data {
 
@@ -23,6 +24,7 @@ public class Mongo implements Data {
 		pr.load(new FileInputStream("mongo.ini"));
 		MongoClient mongo = new MongoClient(pr.getProperty("server"), Integer.parseInt(pr.getProperty("port")));
 		this.db = mongo.getDatabase(pr.getProperty("db"));
+
 	}
 
 	@Override
@@ -39,19 +41,36 @@ public class Mongo implements Data {
 
 	@Override
 	public void deleteCoche(Coche ch) {
-		// TODO Apéndice de método generado automáticamente
+	
+
+		MongoCollection<Document> data = db.getCollection("coches");
+		FindIterable<Document> cursor = data.find();
+
+		for (Document document : cursor) {
+			if (document.getInteger("ID") == ch.getId()) {
+				//document.remove();
+			}else {
+				System.out.println("Nacho es gei jajaja");
+			}
+		}
 
 	}
 
 	@Override
 	public HashMap<Integer, Coche> getCoche() throws SQLException, FileNotFoundException {
 		HashMap<Integer, Coche> result = new HashMap<Integer, Coche>();
-		MongoCollection<Document> data = db.getCollection("marcas");
+		MongoCollection<Document> data = db.getCollection("coches");
 		FindIterable<Document> cursor = data.find();
 		int i = 0;
 		for (Document x : cursor) {
-			//result.put(i, new Coche(x.getInteger("ID"), x.getString("Nombre"), x.getString("Descripcion"),x.getString("Caracteristica1"),x.getString("Caracteristica2"),x.getObje));
+
+			Document tmpMarca = (Document) x.get("Marca");
+			Marca mc = new Marca(tmpMarca.getInteger("ID_Fabricante"), tmpMarca.getString("Nombre"),
+					tmpMarca.getString("Sede"));
+			result.put(i, new Coche(x.getInteger("ID"), x.getString("Nombre"), x.getString("Descripcion"),
+					x.getString("Caracteristica1"), x.getString("Caracteristica2"), mc));
 			i++;
+
 		}
 		return result;
 	}
@@ -70,7 +89,9 @@ public class Mongo implements Data {
 
 	@Override
 	public void deleteMarca(Marca mc) {
-		// TODO Apéndice de método generado automáticamente
+		HashMap<Integer, Marca> result = new HashMap<Integer, Marca>();
+		MongoCollection<Document> data = db.getCollection("marcas");
+		FindIterable<Document> cursor = data.find();
 	}
 
 	@Override
